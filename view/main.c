bdbb9c717672073272648b440c91594fe57ed800
jnegroITBA <jnegro@gmail.com>
2021-09-11 20:17:59 -0300

    First View Process

--- view/main.c
+++ view/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main() {
-    printf("hola\n");
+
+int main(int argc, char const *argv[]) {
+    char * shmName;
+    int shmSize;
+    size_t shm_fd;
+    void * ptr;
+
+    if (argc > 1)
+    {
+        shmName = argv[1];
+    }
+    else
+    {
+        scanf("%s %s", shmName, shmSize);
+    }
+
+
+    shm_fd = shm_open(shmName, O_RDONLY, 0);
+    ptr = mmap(NULL, shmSize, PROT_READ, MAP_SHARED, shm_fd, 0);
+
+
+    return 0;
 }
+
