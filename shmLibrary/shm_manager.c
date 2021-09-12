5084feceeefcd0ba5f7ef427727b114a8f1a22c6
msambartolomeo <msambartolomeo@itba.edu.ar>
2021-09-12 20:09:43 -0300

    ShmTAD test2

---
+++ shmLibrary/shm_manager.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <string.h>
+#include "shm_manager.h"
+
+#define SHM_NAME "/THEBIGSHM"
+
+typedef struct ShmData {
+    sem_t * sem;
+    char * data;
+} ShmData;
+
+shmPointer create_shm(int fileCount) 
+{
+    int shmfd = shm_open(SHM_NAME, O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IRGRP);
+    if (shmfd < 0)
+    {
+        perror("shm_open");
+        exit(1);
+    }
+
+    if (ftruncate(shmfd,PIPE_BUF*fileCount) != 0)
+    {
+        perror("ftruncate");
+        exit(1);
+    }
+
+    void * shm = mmap(NULL,PIPE_BUF*fileCount, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+
+    if (shm == MAP_FAILED)
+    {
+        perror("mmap");
+        exit(1);
+    }
+
+    close(shmfd);
+    
+    sem_init((sem_t *) shm, 1, 0); 
+
+    shmPointer res = malloc(sizeof(ShmData));
+    res->data = (char *) shm + sizeof(sem_t);
+    res->sem = (sem_t *) shm;
+
+    return res;
+}
+
+char * getData(shmPointer shm) 
+{
+    return shm->data;
+}
+
+void waitsem(shmPointer shm)
+{
+    sem_wait(shm->sem);
+}
+
+void postsem(shmPointer shm)
+{
+    sem_post(shm->sem);
+}
+
+void destroy_shm(shmPointer shm) 
+{
+    sem_destroy((sem_t *) shm);
+    munmap(shm, sizeof(shm));
+    shm_unlink(SHM_NAME);
+    free(shm);
+}
+
---
+++ shmLibrary/shm_manager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define SHM_NAME_LEN 128
+
+typedef struct ShmData * shmPointer;
+
+shmPointer create_shm(int fileCount);
+
+void destroy_shm();
+
+shmPointer attach_shm(const char * name);
+
+void dettach_shm(shmPointer shm);
+
+char * getData(shmPointer shm) ;
+
+void waitsem(shmPointer shm);
+
+void postsem(shmPointer shm);
